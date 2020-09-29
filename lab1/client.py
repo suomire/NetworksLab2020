01a22bf79581977ca810b815ffea17490fb2d3ed
suomire <olllkarpushkina@gmail.con>
2020-09-29 12:14:49 +0300

    client-server structure working, but console UI isn't good looking

--- lab1/client.py
+++ lab1/client.py
@@ -1,15 +1,19 @@
 import socket
 import threading
+import sys
+
+# TODO add timestamp !!
 
 HEADER_LEN = 10
 my_username = input("Username: ")
+WORK_SESSION = True
 
 client_socket = socket.socket(
     socket.AF_INET,
     socket.SOCK_STREAM
 )
 
-client_socket.connect(("127.0.0.1", 1236))
+client_socket.connect(("127.0.0.1", 1234))
 
 username = my_username.encode('utf-8')
 username_msg = f"{len(username):<{HEADER_LEN}}".encode('utf-8') + username
@@ -17,13 +21,15 @@ client_socket.send(username_msg)
 
 
 def listen_server():
+    global WORK_SESSION
     while True:
         try:
 
             message_header = client_socket.recv(HEADER_LEN)
             if not len(message_header):
                 print("Connection closed by the server")
-                sys.exit()
+                WORK_SESSION = False
+                sys.exit(0)
 
             message_len = int(message_header.decode('utf-8').strip())
             message = client_socket.recv(message_len).decode('utf-8')
@@ -33,17 +39,17 @@ def listen_server():
         except IOError as e:
             if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
                 print('Reading error: {}'.format(str(e)))
-                sys.exit()
-
+                sys.exit(-1)
                 continue
+
 
 def send_server():
     listen_thread = threading.Thread(target=listen_server)
     listen_thread.start()
-    while True:
+    while WORK_SESSION:
         message = input(f'{my_username} << \n')
         if message:
-            message = my_username +' > ' + message
+            message = '\t' + my_username + ' > ' + message
             message = f'{len(message):<{HEADER_LEN}}' + message
             client_socket.send(message.encode('utf-8'))
 
--- lab1/server.py
+++ lab1/server.py
@@ -2,7 +2,7 @@ import socket
 from threading import Thread
 
 IP = "127.0.0.1"
-PORT = 1236
+PORT = 1234
 HEADER_LEN = 10
 
 server_socket = socket.socket(
@@ -17,41 +17,49 @@ server_socket.listen(5)
 print("Server is listening")
 
 clients = {}
-chat_users = {}
 
 
 def broadcast(msg):
     """
     Send message to all chat users
-    msg in bytes!
+    msg in str!
     """
-    msg = msg.decode('utf-8')
+    msg_header = f"{len(msg):<{HEADER_LEN}}"
+    msg = msg_header + msg
+
     for client in clients.keys():
-        msg_header = f"{len(msg):<{HEADER_LEN}}"
-        msg = msg_header + msg
         client.send(msg.encode('utf-8'))
 
 
 def handle_client(client):
+    """
+    Handling working with accepted clientself.
+    1. Receiving client's name
+    2. Sending info message
+    3. Send broadcast message about new chat member
+    4. Working with receiving new messages from this client
+
+    """
     name_header_len = int(client.recv(HEADER_LEN).decode('utf-8').strip())
     name = client.recv(name_header_len).decode('utf-8')
 
-    msg_welcome = "Hi, %s! To quit from chat type {quit} \n" % name
+    msg_welcome = "Hi, %s! To quit from chat type <quit< \n" % name
     msg_welcome = "Server >" + msg_welcome
     msg_welcome = f"{len(msg_welcome):<{HEADER_LEN}}" + msg_welcome
 
     client.send(msg_welcome.encode('utf-8'))
 
-    msg_broadcast = "%s has joined to the chat! \n" % name
-    broadcast(msg_broadcast.encode('utf-8'))
+    msg_broadcast = "\t %s has joined to the chat! \n" % name
+    broadcast(msg_broadcast)
 
-    chat_users[client] = name
 
     while True:
         msg_len = int(client.recv(HEADER_LEN).decode('utf-8').strip())
-        msg = client.recv(msg_len)
+        msg = client.recv(msg_len).decode('utf-8')
 
-        if msg.decode('utf-8') != "{quit}".encode("utf-8"):
+        print(msg)
+
+        if msg.split(' > ')[-1] != "<quit<":
             broadcast(msg)
         else:
             exit_msg = "You exit from chat."
@@ -60,21 +68,21 @@ def handle_client(client):
             client.send(exit_msg.encode('utf-8'))
             client.close()
             del clients[client]
-            del chat_users[client]
             msg_bye = "%s has left the chat." % name
-            broadcast(msg_bye.encode("utf-8"))
+            broadcast(msg_bye)
             break
 
 
 def accept_incoming_connection():
     """
-    Handling new clients:
-    accept, add to client_dict, start a new thread
-
+    Handling incoming connections:
+    accept, send welcome message,
+    add to client_dict, start a new thread
     """
     while True:
         client, client_address = server_socket.accept()
         print("%s:%s has connected." % client_address)
+
         msg = "Welcome to chat!\n"
         msg = f"{len(msg):<{HEADER_LEN}}" + msg
         client.send(msg.encode('utf-8'))
